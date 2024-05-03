e7f9066c0bf746b368eb9e71558dd25203abfdea
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2024-05-02 17:24:46 -0800

    plan to switch topic sel to carousel

--- js/script.js
+++ js/script.js
@@ -131,6 +131,49 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+//TODO: create carousel for the topic selection.
+// const prevBtn = document.getElementById('prevBtn');
+//    const nextBtn = document.getElementById('nextBtn');
+//    const slide = document.querySelector('#work-experience .carousel-slide');
+//    const cards = document.querySelectorAll('#work-experience .card');
+//    let windowWidth = window.innerWidth;
+//    let carouselCounter = 0;
+//    let totalSwipes = 0;
+//    const carouselSize = cards[0].clientWidth + 16; // Card width + margin-right.
+
+//    // size of window determines increments of carousel counter.
+//    nextBtn.addEventListener('click', () => {
+//     if (totalSwipes >= cards.length - 1) {
+//         return;
+//     } 
+//     slide.style.transition = "transform  0.5s ease-in-out";
+//     carouselCounter++;
+//     slide.style.transform = "translateX(" + (-carouselSize * carouselCounter) + "px)";
+//     // console.log(-carouselSize * carouselCounter);
+//     // console.log(totalSwipes);
+//     if (windowWidth >= 1275) { 
+//         totalSwipes += 2;
+//     } else if (windowWidth >= 960) {
+//         totalSwipes += 1.5;
+//     } else {
+//         totalSwipes++;
+//     }
+//    })
+
+//    prevBtn.addEventListener('click', () => {
+//     if (totalSwipes <= 0) return;
+//     slide.style.transition = 'transform 0.5s ease-in-out';
+//     carouselCounter--;
+//     slide.style.transform = 'translateX(' + (-carouselSize * carouselCounter) + 'px)';
+//     if (windowWidth >= 1275) { 
+//         totalSwipes += -2;
+//     } else if (windowWidth >= 960) {
+//         totalSwipes += -1.5;
+//     } else {
+//         totalSwipes--;
+//     }
+//    })
+
 
 /*
 
